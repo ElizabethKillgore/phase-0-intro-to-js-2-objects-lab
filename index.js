9d9c24bd1e1c86df56711a69e94de020b5ec98ef
Elizabeth <ek@elizabethkillgore.com>
2023-05-31 17:13:17 -0500

    EK 7 passing

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your solution in this file!
+
+const employee = {
+   "name": "Liz Scott", 
+   "streetAddress": {
+      street: "315 Emerald Rd",
+      city: "New York",
+      state: "NY",
+      zip: "10021" 
+   },
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+   
+   const newObj = { ...obj};
+
+   newObj[key] = value;
+
+   return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+
+
+
+   obj[key] = value;
+
+   return obj;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+
+   const newEmployee = {...employee};
+   
+   delete  newEmployee[key]
+
+   return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+
+delete employee[key] 
+
+return employee
+}
